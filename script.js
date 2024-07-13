e30b32feb8197aef07dc323dbf27e1884a909a23
Nurlan Jalilov <143971330+nurlanjalil@users.noreply.github.com>
2024-07-14 01:29:08 +0400

    hamburger-nav

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const body = document.body;
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  body.classList.toggle("menu-open");
 }
